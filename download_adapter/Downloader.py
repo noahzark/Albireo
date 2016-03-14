035b3f57ea10592cd527de72f9ceb5dc9cf66303
lordfriend <lordfriend@outlook.com>
2016-03-14 21:25:39 +0800

    add DelugeDownloader

---
+++ download_adapter/Downloader.py
@@ -0,0 +1,14 @@
+
+
+class Downloader:
+
+    def download(self, eps):
+        pass
+
+class DownloadEventListener:
+
+    def on_download_completed(self):
+        pass
+
+    def on_download_failed(self):
+        pass
