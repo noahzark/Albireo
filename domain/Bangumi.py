209518ed6d615ee4629b3af3ecc218021861cb2d
lordfriend <lordfriend@outlook.com>
2016-03-16 00:53:49 +0800

    add __init__.py to each sub directories

--- domain/Bangumi.py
+++ domain/Bangumi.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, TEXT, DATE
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import relationship
-from domain.Episode import Episode
+from Episode import Episode
 
 Base = declarative_base()
 
