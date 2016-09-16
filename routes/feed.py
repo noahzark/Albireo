56f607bcc7ca9abe3b6a1551a894c9a63cd65394
lordfriend <lordfriend@outlook.com>
2016-09-16 22:39:11 +0800

    add a new api for search feed from dmhy

---
+++ routes/feed.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flask import request, Blueprint
+
+from service.feed import feed_service
+from flask_login import login_required
+from service.auth import auth_user
+from utils.exceptions import ClientError
+from domain.User import User
+
+feed_api = Blueprint('feed', __name__)
+
+
+@feed_api.route('/dmhy/<keywords>', methods=['GET'])
+@login_required
+@auth_user(User.LEVEL_ADMIN)
+def dmhy(keywords):
+    if keywords is None:
+        raise ClientError('keywords is empty', 400)
+    else:
+        return feed_service.parse_dmhy(keywords)
