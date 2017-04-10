5cacd294b1dbc4171ee39afc62c86f42df144fbb
lordfriend <lordfriend@outlook.com>
2017-04-10 22:20:39 +0800

    add bangumi moe proxy api and bangumi torrent search api

--- utils/http.py
+++ utils/http.py
@@ -10,6 +10,8 @@ import logging
 import traceback
 import pickle
 
+from requests import Request
+
 logger = logging.getLogger(__name__)
 
 user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0'
@@ -98,4 +100,51 @@ class BangumiRequest:
         self.__save_cookie_to_storage()
         return r
 
+
+class BangumiMoeRequest:
+
+    def __init__(self):
+
+        # persist request for accessing bangumi api
+        self.session = requests.Session()
+        self.session.headers.update({
+            'User-Agent': user_agent,
+            'Accept': 'application/json;charset=utf-8'
+        })
+
+        session_storage_path = './.session'
+        self.api_bgm_tv_session_path = session_storage_path + '/bangumi_moe'
+
+        try:
+            if not os.path.exists(session_storage_path):
+                os.makedirs(session_storage_path)
+                logger.info('create session storage dir %s successfully' % session_storage_path)
+        except OSError as exception:
+            if exception.errno == errno.EACCES:
+                raise exception
+            else:
+                logger.warn(exception)
+
+    def __get_cookie_from_storage(self):
+        try:
+            with open(self.api_bgm_tv_session_path, 'r') as f:
+                self.session.cookies = requests.utils.cookiejar_from_dict(pickle.load(f))
+        except Exception as error:
+            logger.warn(traceback.format_exc(error))
+
+    def __save_cookie_to_storage(self):
+        try:
+            with open(self.api_bgm_tv_session_path, 'w') as f:
+                pickle.dump(requests.utils.dict_from_cookiejar(self.session.cookies), f)
+        except Exception as error:
+            logger.warn(traceback.format_exc(error))
+
+    def post(self, url, payload):
+        self.__get_cookie_from_storage()
+        r = self.session.post(url=url, json=payload)
+        self.__save_cookie_to_storage()
+        return r
+
+
 bangumi_request = BangumiRequest()
+bangumi_moe_request = BangumiMoeRequest()
