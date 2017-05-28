c2260c51f9576c13899f3d2dc7a25e43fab9c988
lordfriend <lordfriend@outlook.com>
2017-05-28 17:38:04 +0800

    hotfix for alembic scripts 3ffc63f8e34f, which may fail when colorthief raise an error. Add a tools to fix the reset

--- utils/color.py
+++ utils/color.py
@@ -1,7 +1,7 @@
 from colorthief import ColorThief
 
 
-def get_dominant_color(path):
+def get_dominant_color(path, quality=1):
     color_thief = ColorThief(path)
-    (r, g, b) = color_thief.get_color(quality=1)
+    (r, g, b) = color_thief.get_color(quality=quality)
     return '#{0:02x}{1:02x}{2:02x}'.format(r, g, b)
